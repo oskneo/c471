8c63844a8adbfe32cb880902c9021402d2da2b43
osk666 <osk666@gmail.com>
2018-07-02 04:37:07 +0000

    hostname

--- client.c
+++ client.c
@@ -2,9 +2,61 @@
 #include<sys/socket.h> 
 #include<arpa/inet.h>
 #include <string.h>
+#include<netdb.h>
+
+#define port4 33455
+#define port6 33446
+#define buffersize6 1280
+#define buffersize4 1440
+#define fn "fileToTransfer"
+#define address "localhost"
  
 int main(int argc , char *argv[])
 {
+    
+    int pt6=port6,
+    pt4=port4,
+    pt,
+    buffer=buffersize6;
+    
+    char hn[50]=address;
+    
+    char msg[30];
+    bzero(msg,30);
+    strcpy(msg,fn);
+    
+    int i;
+    for(i=1;i<argc;i++){
+        if(strcmp(argv[i],"-f")==0){
+            bzero(msg,30);
+            strcpy(msg,argv[i+1]);
+        }
+        else if(strcmp(argv[i],"-p6")==0){
+            pt6=atoi(argv[i+1]);
+        }
+        else if(strcmp(argv[i],"-p4")==0){
+            pt4=atoi(argv[i+1]);
+        }
+        else if(strcmp(argv[i],"-b")==0){
+            buffer=atoi(argv[i+1]);
+        }
+        else if(strcmp(argv[i],"-a")==0){
+            bzero(hn,50);
+            strcpy(hn,argv[i+1]);
+        }
+    }
+    
+    pt=pt4;
+    
+    
+    
+    struct hostent *adr=gethostbyname(hn);
+    if(adr==NULL){
+        herror("gethostbyname");
+        return 1;
+    }
+    
+    
     int socket_desc;
     struct sockaddr_in server;
     
@@ -17,20 +69,20 @@ int main(int argc , char *argv[])
     else{
         printf("Create socket successfully.\n");   
     }
-    
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)adr->h_addr_list[0])));
     server.sin_family = AF_INET;
-    server.sin_port= htons(33445);
+    server.sin_port= htons(pt);
     
     if(connect(socket_desc, (struct sockaddr *)&server, sizeof(server))<0){
-        printf("Connection fail.\n");
+        printf("Connection failed.\n");
         return 1;
     }
     else{
         printf("Connect successfully.\n");
     }
     
-    char* msg="Hiiiiiii!\n";
+    
+    
     // write(newsocket,nmsg,strlen(nmsg));
     
     
@@ -45,15 +97,76 @@ int main(int argc , char *argv[])
         printf("Sending successfully.\n");
     }
     
-    char rmsg[1024];
-    if(recv(socket_desc,rmsg,1024,0)<0){
+    // char rmsg[1024];
+    // recv(socket_desc,rmsg,1024,0
+    
+    
+    receivefile(socket_desc,msg, buffer);
+    close(socket_desc);
+    
+     
+    return 0;
+}
+
+
+int receivefile(int nsocket,char * msg,int bs){
+    
+    int reno=0;
+    
+    if(recv(nsocket,&reno,4,0)<0){
         printf("Receiving fail.\n");
     }
     else{
         printf("Receiving successfully.\n");
-        printf("%s\n",rmsg);
+        // printf("%s\n",rmsg);
+        // char nmsg[100];
+        // strcpy(nmsg,"The size of file is ");
+        // strcat(nmsg, itoa(filesize));
+        // strcat(nmsg, ".\n");
+        
+        if(reno==-1){
+            printf("COULD NOT OPEN REQUESTED FILE!\n");
+            return -1;
+            
+        }
+        
+        
+        printf("The size of file is %d.\n",reno);
     }
     
-     
+    FILE *file=fopen(msg,"a");
+    char buf[bs];
+    bzero(&buf,bs);
+    int received=bs;
+    
+    while(reno>0){
+        
+        if(reno<bs){
+            received=reno;
+        }
+        recv(nsocket,buf,received,0);
+        // printf("%d\n",reno);
+        if(received<0){
+            printf("Receiving fail.\n");
+            fclose(file);
+            return 1;
+        }
+        // else if(received==0){
+        //     break;
+        // }
+        else{
+            
+            fwrite(buf,1,received,file);
+            reno-=received;
+            bzero(&buf,bs);
+            
+        }
+    }
+    fclose(file);
+    printf("Receiving successfully.\n");
+    
     return 0;
+    
+    
+    
 }
---
+++ file/server.c
@@ -0,0 +1,320 @@
+#include<stdio.h>
+#include<sys/socket.h> 
+#include<arpa/inet.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+#define queue 20
+#define port4 33455
+#define port6 33446
+#define buffersize6 1280
+#define buffersize4 1440
+#define fn "fileToTransfer"
+
+
+int connection46(int port, int buffer,int type){
+    
+    int socket_desc;
+    struct sockaddr_in server, client;
+    struct sockaddr_in6 server6, client6;
+    
+    void * sv;
+    void * cl;
+    
+    if(type==0){
+        socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+        
+        bzero(&server,sizeof(server));
+        server.sin_addr.s_addr = INADDR_ANY;
+        server.sin_family = AF_INET;
+        server.sin_port= htons(port);
+        
+        sv=&server;
+    }
+    else{
+        socket_desc = socket(AF_INET6 , SOCK_STREAM , 0);
+        
+        int on=1;
+        if (setsockopt(socket_desc, IPPROTO_IPV6, IPV6_V6ONLY, &on, sizeof(on)) < 0) {
+            perror("IPV6_V6ONLY setting failed.");
+            exit(1);
+        }
+        bzero(&server6,sizeof(server6));
+        server6.sin6_addr = in6addr_any;
+        server6.sin6_family = AF_INET6;
+        server6.sin6_port= htons(port);
+        
+        sv=&server6;
+    }
+    
+    
+     
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket\n");
+        
+        return -1;
+    }
+    else{
+        printf("Create socket successfully.\n");   
+    }
+    
+    
+    if(bind(socket_desc, type==0 ? (struct sockaddr *)&server : (struct sockaddr *)&server6, type==0 ? sizeof(server) : sizeof(server6))<0){
+        printf("Bind fail.\n");
+        return 1;
+    }
+    else{
+        printf("Bind successfully.\n");
+    }
+    
+    listen(socket_desc, queue);
+    printf("Waiting...\n");
+    int c;
+    c = type==0 ? sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6);
+    int newsocket;
+    while(newsocket = accept(socket_desc, type==0 ? (struct sockaddr *)&client : (struct sockaddr *)&client6, (socklen_t*)&c)){
+        int f=fork();
+        if(f==0){
+            if (newsocket<0){
+                perror("Accept failed.\n");
+                return -1;
+            }
+            else{
+                printf("Accept successfully.\n");
+            }
+    
+    
+    
+            char msg[30];
+            if(read(newsocket,msg,30)<0){
+                printf("Receiving fail.\n");
+                return -1;
+            }
+            else{
+                printf("Receiving successfully.\n");
+                printf("%s\n",msg);
+            }
+    
+    
+            sendfile(newsocket,msg,buffer);
+        }
+        else if(f<0){
+            printf("Child process creating failed.\n");
+            exit(1);
+        }
+    
+    }
+    
+    
+     
+    return 0;
+    
+}
+
+
+
+
+int sendfile(int nsocket,char * msg,int bs){
+    
+    FILE *file=fopen(msg,"r");
+    int filesize=0;
+    if(file==NULL){
+        filesize=-1;
+        write(nsocket,&filesize,4);
+        return -1;
+    }
+    
+    
+    
+    fseek(file, 0L, SEEK_END);
+    filesize = ftell(file);
+    rewind(file);
+    
+    
+    write(nsocket,&filesize,4);
+    
+    char buf[bs];
+    bzero(&buf,bs);
+    while(filesize>0){
+        fread(buf,1,bs,file);
+        
+        if(send(nsocket,buf,bs,0)<0){
+            printf("Sending file failed.\n");
+            fclose(file);
+            return 1;
+        }
+        filesize-=bs;
+        bzero(&buf,bs);
+    }
+    fclose(file);
+    
+    printf("Sending successfully.\n");
+    return 0;
+    
+    
+    
+    
+    
+    
+}
+ 
+int main(int argc , char *argv[])
+{
+    int pt6=port6,
+    pt4=port4,
+    bfa[2]={buffersize4,buffersize6};
+    
+    
+    int i;
+    for(i=1;i<argc;i++){
+        if(strcmp(argv[i],"-p6")==0){
+            pt6=atoi(argv[i+1]);
+        }
+        if(strcmp(argv[i],"-p4")==0){
+            pt4=atoi(argv[i+1]);
+        }
+        if(strcmp(argv[i],"-b")==0){
+            bfa[0]=atoi(argv[i+1]);
+            bfa[1]=atoi(argv[i+1]);
+        }
+    }
+    
+    int pt[2]={pt4,pt6};
+    
+    
+    // int socket_desc;
+    // struct sockaddr_in server, client;
+    
+    // socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+     
+    // if (socket_desc == -1)
+    // {
+    //     printf("Could not create socket\n");
+    // }
+    // else{
+    //     printf("Create socket successfully.\n");   
+    // }
+    
+    // server.sin_addr.s_addr = INADDR_ANY;
+    // server.sin_family = AF_INET;
+    // server.sin_port= htons(pt);
+    
+    // if(bind(socket_desc, (struct sockaddr *)&server, sizeof(server))<0){
+    //     printf("Bind fail.\n");
+    //     return 1;
+    // }
+    // else{
+    //     printf("Bind successfully.\n");
+    // }
+    
+    // listen(socket_desc, queue);
+    // printf("Waiting...\n");
+    // int c = sizeof(struct sockaddr_in);
+    // int newsocket;
+    // while(newsocket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)){
+    //     int f=fork();
+    //     if(f==0){
+    //         if (newsocket<0){
+    //             perror("Accept failed.\n");
+    //             return -1;
+    //         }
+    //         else{
+    //             printf("Accept successfully.\n");
+    //         }
+    
+    
+    
+    //         char msg[30];
+    //         if(read(newsocket,msg,30)<0){
+    //             printf("Receiving fail.\n");
+    //             return -1;
+    //         }
+    //         else{
+    //             printf("Receiving successfully.\n");
+    //             printf("%s\n",msg);
+    //         }
+    
+    
+    //         sendfile(newsocket,msg,buffer);
+    //     }
+    //     else if(f<0){
+    //         printf("Child process creating failed.\n");
+    //         return -1;
+    //     }
+    
+    // }
+    
+    
+     
+    // return 0;
+    
+    int processId[2],pid;
+    for(i=0;i<1;i++){
+        pid=fork();
+        if(pid==0){
+            
+            // printf("%d\n",getpid());
+            int buffer=bfa[i];
+            if(connection46(pt[i],buffer,i)!=0){
+                exit(1);
+            }
+        }
+        else if(pid<0){
+            switch(i){
+                case 0:
+                    puts("The process to build ipv4 connection generated failed.");
+                    break;
+                case 1:
+                    puts("The process to build ipv6 connection generated failed.");
+            }
+            
+        }
+        else{
+        
+            processId[i]=pid;
+            
+        }
+        
+    }
+    int status;
+    for(i=0;i<1;i++){
+        
+        waitpid(processId[i], &status, 0);
+        // switch(status){
+            
+        // }
+    }
+    
+    // int pc6;
+    // int pc4=fork();
+    // if(pc4==0){
+    //     if(connection4(pt,buffer)!=0){
+    //         exit(1);
+    //     }
+    // }
+    // else if(pc4<0){
+    //     puts("The process to build ipv4 connection generated failed.");
+    // }
+    // else{
+    //     // pc6=fork();
+    //     // if(pc6==0){
+    //     //     if(connection6(pt,buffer)!=0){
+    //     //         exit(1);
+    //     //     }
+    //     // }
+    //     // else if(pc6<0){
+    //     //     puts("The process to build ipv6 connection generated failed.");
+    //     // }
+    //     // else{
+        
+    //     // }
+    // }
+    
+    printf("%d\n",processId[0]);
+    
+    return 0;
+}
+
+
