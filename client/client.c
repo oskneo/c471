8365da58631b98c7b53115f7bb68e1a784f256ad
osk666 <osk666@gmail.com>
2018-07-04 00:46:14 +0000

    final

--- client/client.c
+++ client/client.c
@@ -20,17 +20,13 @@
 int receivefile(int nsocket,char * msg,int bs){
     
     int reno=0;
+    //Initiate the variable for the size of the requested file.
     
     if(recv(nsocket,&reno,4,0)<0){
         printf("Receiving fail.\n");
     }
     else{
         printf("Receiving successfully.\n");
-        // printf("%s\n",rmsg);
-        // char nmsg[100];
-        // strcpy(nmsg,"The size of file is ");
-        // strcat(nmsg, itoa(filesize));
-        // strcat(nmsg, ".\n");
         
         if(reno==-1){
             printf("COULD NOT OPEN REQUESTED FILE!\n");
@@ -41,21 +37,23 @@ int receivefile(int nsocket,char * msg,int bs){
         
         printf("The size of file is %d.\n",reno);
     }
+    //Assign the file size received from server to this variable.
     
+    
+    
+    //Build a file to write data received from server on it.
     FILE *file=fopen(msg,"w");
     rewind(file);
     char buf[bs];
     bzero(buf,bs);
     int received=bs;
     
+    //Use a while loop to receive fragments of the file and write them on the file built above.
     while(reno>0){
         
-        // if(reno<bs){
-        //     received=reno;
-        // }
         printf("reno:%d\n",reno);
         received=recv(nsocket,buf,bs,0);
-        printf("received:%d\n",received);
+        //The fragments of the file will be received and written to buffer first. 
         
         if(received<0){
             printf("Receiving fail.\n");
@@ -69,10 +67,13 @@ int receivefile(int nsocket,char * msg,int bs){
             if(reno<bs){
                 received=reno;
             }
-            
+            printf("received:%d\n",received);
+            //Then write to the file on disk.
             fwrite(buf,received,1,file);
             reno-=received;
             bzero(buf,bs);
+            //After finishing writing one buffer,
+            //clear the buffer and wait for receiving another fragment.
             
         }
     }
@@ -90,7 +91,7 @@ int sock(char * hn, int pt[2], int *buffer,int bfa[2]){
 
     struct addrinfo *rs;
     struct addrinfo indication;
-    // int family;
+    //Initiate socket file descripter, address info, server and client struct for setting connections.
 
 
     int socket_desc,errno=0;
@@ -101,20 +102,20 @@ int sock(char * hn, int pt[2], int *buffer,int bfa[2]){
         printf("Cannot get the ip address from the host name.");
         return -1;
     }
+    //Get the address from host name. 
+    //This address contains the type of IP connection.
+    //Then the function can deal with IPv4 and IPv6 seperately.
 
     switch(rs->ai_family){
+        //When ai_family is equal to AF_INET, it is IPv4.
         case AF_INET:{
             puts("This is an IPv4 address.");
             
             
-            struct ifreq ifr; 
-            ifr.ifr_addr.sa_family = AF_INET;
-            //iap->ifa_name is bond1:xx
-            strcpy(ifr.ifr_name, "eth1");
-            ifr.ifr_mtu = 1280; 
 
             struct sockaddr_in *server4;
             socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+            //Create an IPv4 TCP socket.
      
             if (socket_desc == -1)
             {
@@ -124,59 +125,40 @@ int sock(char * hn, int pt[2], int *buffer,int bfa[2]){
                 printf("Create socket successfully.\n");   
             }
             
+            //Use the address above to set the connection.
             
             
             
             
-            
-            
-            
-            
-            
-            
-            
-            
-            
-    //server.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)adr->h_addr_list[0])));
-            // server4.sin_addr.s_addr = inet_addr("::1");
-    
-            // server4.sin_family = AF_INET;
-            // server4.sin_port= htons(pt[0]);
-            // puts("gggggg");
             server4 = (struct sockaddr_in *)rs->ai_addr;
-            // server4->sin_addr.s_addr=inet_addr(rs->ai_addr->sa_data);
             server4->sin_port=htons(pt[0]);
-            // server4->sin_family=AF_INET;
-            // puts("gggggg1");
             *buffer=bfa[0];
+            //Let buffer be the size set for IPv4.
 
             errno=connect(socket_desc, (struct sockaddr *)server4, sizeof(*server4));
-            // errno=connect(socket_desc, rs->ai_addr, rs->ai_addrlen);
-            
-            // puts("gggggg2");
-            // ioctl(socket_desc, SIOCSIFMTU, (caddr_t)&ifr);
+            //Build the IPv4 connection to server using the settings.
             
             int socket_mtu=0;
             int sz=sizeof(socket_mtu);
             getsockopt(socket_desc,IPPROTO_IP,IP_MTU,(char *)&socket_mtu, &sz );
-            printf("MTU --> %d\n",socket_mtu); 
+            printf("Original MTU is %d.\n",socket_mtu); 
+            //Check the MTU of this connetion.
+            
+            
+            
             
 
             break;
         }
+        //When ai_family is equal to AF_INET6, it is IPv6.
         case AF_INET6:{
             puts("This is an IPv6 address.");
 
-            struct ifreq ifr; 
-            ifr.ifr_addr.sa_family = AF_INET6;
-            //iap->ifa_name is bond1:xx
-            strcpy(ifr.ifr_name, "eth1");
-            ifr.ifr_mtu = 1280; 
             
 
             struct sockaddr_in6 *server6;
             socket_desc = socket(AF_INET6 , SOCK_STREAM , 0);
-
+            //Create an IPv6 TCP socket.
             
      
             if (socket_desc == -1)
@@ -188,11 +170,6 @@ int sock(char * hn, int pt[2], int *buffer,int bfa[2]){
             }
 
             
-    //server.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)adr->h_addr_list[0])));
-            // server4.sin_addr.s_addr = inet_addr("::1");
-    
-            // server4.sin_family = AF_INET;
-            // server4.sin_port= htons(pt[0]);
 
             int on=1;
             if (setsockopt(socket_desc, IPPROTO_IPV6, IPV6_V6ONLY, &on, sizeof(on)) < 0) {
@@ -202,32 +179,33 @@ int sock(char * hn, int pt[2], int *buffer,int bfa[2]){
             
 
             
-            // puts("ggggg");
-            // server6=(struct sockaddr_in6*)malloc(sizeof(struct sockaddr_in6));
-            // bzero(server6,sizeof(*server6));
-            // memcpy(&server6, rs->ai_addr, rs->ai_addrlen);
-            // puts("ggggg1");
+            //Use the address above to set the connection.
             server6 = (struct sockaddr_in6 *)rs->ai_addr;
             server6->sin6_port=htons(pt[1]);
             server6->sin6_family=AF_INET6;
-            // puts("ggggg2");
             *buffer=bfa[1];
+            //Let buffer be the size set for IPv6.
             errno=connect(socket_desc, (struct sockaddr *)server6, sizeof(*server6));
-            // puts("ggggg3");
+            //Build the IPv4 connection to server using the settings.
             
-            ioctl(socket_desc, SIOCSIFMTU, (caddr_t)&ifr);
             
-            int mtu=1280;
-            if (setsockopt(socket_desc, IPPROTO_IPV6, IPV6_MTU, &mtu, sizeof(mtu)) < 0) {
-                perror("IPV6_MTU setting failed.");
-                exit(1);
-            }
             
             
             int socket_mtu=0;
             int sz=sizeof(socket_mtu);
             getsockopt(socket_desc,IPPROTO_IPV6,IPV6_MTU,(char *)&socket_mtu, &sz );
-            printf("MTU --> %d\n",socket_mtu); 
+            printf("Original MTU is %d.\n",socket_mtu); 
+            //Check the MTU of this connetion.
+            
+            int mtu=*buffer+40;
+            if(mtu>=1280){
+                if (setsockopt(socket_desc, IPPROTO_IPV6, IPV6_MTU, &mtu, sizeof(mtu)) < 0) {
+                    perror("IPV6_MTU setting failed.");
+                    exit(1);
+                }
+            }
+            //Set the MTU using buffer size.
+            
 
         }
 
@@ -240,7 +218,14 @@ int sock(char * hn, int pt[2], int *buffer,int bfa[2]){
     else{
         printf("Connect successfully.\n");
     }
-
+    
+    if(send(socket_desc,buffer,4,0)<0){
+        printf("Sending failed.\n");
+    }
+    else{
+        printf("Sending new buffer size successfully.\n");
+    }
+    //Send the buffer size to server.
 
 
 
@@ -255,6 +240,7 @@ int main(int argc , char *argv[])
     pt[2],
     bfa[2]={buffersize4,buffersize6},
     buffer=0;
+    //Initiate variables for setting defaults.
     
     char hn[50]=address;
     
@@ -264,27 +250,27 @@ int main(int argc , char *argv[])
     
     int i;
     for(i=1;i<argc;i++){
-        if(strcmp(argv[i],"-f")==0){
+        if(i==4){
             bzero(msg,30);
-            // memset(msg,0,30);
-            strcpy(msg,argv[i+1]);
-            // memcpy(msg,argv[i+1],strlen(argv[i+1]));
+            strcpy(msg,argv[i]);
         }
-        else if(strcmp(argv[i],"-p6")==0){
-            pt6=atoi(argv[i+1]);
+        else if(i==2){
+            pt6=atoi(argv[i]);
+            pt4=atoi(argv[i]);
         }
-        else if(strcmp(argv[i],"-p4")==0){
-            pt4=atoi(argv[i+1]);
+        else if(i==5){
+            bfa[0]=atoi(argv[i]);
+            bfa[1]=atoi(argv[i]);
         }
-        else if(strcmp(argv[i],"-b")==0){
-            bfa[0]=atoi(argv[i+1]);
-            bfa[1]=atoi(argv[i+1]);
-        }
-        else if(strcmp(argv[i],"-a")==0){
+        else if(i==1){
             bzero(hn,50);
-            strcpy(hn,argv[i+1]);
+            strcpy(hn,argv[i]);
         }
     }
+    
+    //Receive arguments to change defaults.
+    //An example of command of using this program:
+    // ./client.o ::1 33446 localhost 1.txt 1460
     
     pt[0]=pt4;
     pt[1]=pt6;
@@ -293,19 +279,13 @@ int main(int argc , char *argv[])
     
     
     
-    // struct hostent *adr=gethostbyname(hn);
-    // if(adr==NULL){
-    //     herror("gethostbyname");
-    //     return 1;
-    // }
-    
-    
+    //Initiate socket file descripter for setting connections.
     int socket_desc;
-    // struct sockaddr_in server;
 
     socket_desc = sock(hn,pt,&buffer,bfa);
+    //It is a function to create socket connection and return a socket file descripter
+    //passing host name, port numbers and buffer sizes.
     
-    // socket_desc = socket(AF_INET , SOCK_STREAM , 0);
      
     if (socket_desc == -1)
     {
@@ -315,23 +295,8 @@ int main(int argc , char *argv[])
     else{
         printf("Create socket successfully.\n");   
     }
-    // //server.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)adr->h_addr_list[0])));
-    // server.sin_addr.s_addr = inet_addr("::1");
     
-    // server.sin_family = AF_INET;
-    // server.sin_port= htons(pt);
-    
-    // if(connect(socket_desc, (struct sockaddr *)&server, sizeof(server))<0){
-    //     printf("Connection failed.\n");
-    //     return 1;
-    // }
-    // else{
-    //     printf("Connect successfully.\n");
-    // }
-    
-    
-    // printf("%lu\n",strlen(msg));
-    
+    //After creating connection successfully, send the file name to sevrer.
     if(send(socket_desc,msg,strlen(msg),0)<0){
         printf("Sending fail.\n");
         return 1;
@@ -340,10 +305,8 @@ int main(int argc , char *argv[])
         printf("Sending successfully.\n");
     }
     
-    // char rmsg[1024];
-    // recv(socket_desc,rmsg,1024,0
     
-    
+    //Then use a function to receive a file from server.
     receivefile(socket_desc,msg, buffer);
     close(socket_desc);
     
--- server/server.c
+++ server/server.c
@@ -21,27 +21,42 @@
 
 int sendfile(int nsocket,char * msg,int bs){
     
+    printf("%s will be sent...\n",msg);
+    
     FILE *file=fopen(msg,"r");
     int filesize=0,sent=0;
     if(file==NULL){
+        printf("COULD NOT OPEN REQUESTED FILE!\n");
         filesize=-1;
         write(nsocket,&filesize,4);
         return -1;
     }
+    //This function will try to open the file depending on the file name sent from the client.
+    //If not able to open the file, sent -1 to client.
     
     
     
     fseek(file, 0L, SEEK_END);
     filesize = ftell(file);
     rewind(file);
+    //If able to open, check the file size and send to client.
     
     
     write(nsocket,&filesize,4);
     
+    
+    //Build a while loop to send fragments of the file.
+    //Each fragment is equal or less than the size of the buffer. 
     char buf[bs];
     bzero(buf,bs);
     while(filesize>0){
         printf("filesize:%d\n",filesize);
+        if(filesize<bs){
+            bs=filesize;
+        }
+        //The loop will first read the size of buffer of the file to the buffer,
+        //then send this content of this buffer to client.
+        
         fread(buf,1,bs,file);
         sent=send(nsocket,buf,bs,0);
         printf("sent:%d\n",sent);
@@ -55,6 +70,7 @@ int sendfile(int nsocket,char * msg,int bs){
         }
         filesize-=sent;
         bzero(buf,bs);
+        //Then clear th buffer.
     }
     fclose(file);
     
@@ -71,58 +87,48 @@ int sendfile(int nsocket,char * msg,int bs){
 
 int connection46(int port, int buffer,int type){
     
+    //Initiate socket file descripter, server and client struct for setting connections.
+    
     int socket_desc;
     struct sockaddr_in server, client;
     struct sockaddr_in6 server6, client6;
     
-    void * sv;
-    void * cl;
 
 
-    struct ifreq ifr; 
-    ifr.ifr_addr.sa_family = AF_INET;
-    //iap->ifa_name is bond1:xx
-    strcpy(ifr.ifr_name, "eth1");
-    ifr.ifr_mtu = 1280; 
     
 
-    
+    //The argument type indicates the type of IP connection.
+    //0 is for IPv4. The other is for IPv6.
     if(type==0){
+        //Create a TCP socket for IPv4.
         socket_desc = socket(AF_INET , SOCK_STREAM , 0);
         
-        ioctl(socket_desc, SIOCSIFMTU, (caddr_t)&ifr);
-        
+        //Setting a IPv4 only connection.
         bzero(&server,sizeof(server));
         server.sin_addr.s_addr = INADDR_ANY;
         server.sin_family = AF_INET;
         server.sin_port= htons(port);
         
-        sv=&server;
         
         
     }
     else{
+        //Create a TCP socket for IPv6.
         socket_desc = socket(AF_INET6 , SOCK_STREAM , 0);
-
-        // ioctl(socket_desc, SIOCSIFMTU, (caddr_t)&ifr);
         
         int on=1;
         if (setsockopt(socket_desc, IPPROTO_IPV6, IPV6_V6ONLY, &on, sizeof(on)) < 0) {
             perror("IPV6_V6ONLY setting failed.");
             exit(1);
         }
+        //Setting an IPv6 only connection.
 
-        // int mtu=1280;
-        // if (setsockopt(socket_desc, IPPROTO_IPV6, IPV6_MTU, &mtu, sizeof(mtu)) < 0) {
-        //     perror("IPV6_MTU setting failed.");
-        //     exit(1);
-        // }
+        
         bzero(&server6,sizeof(server6));
         server6.sin6_addr = in6addr_any;
         server6.sin6_family = AF_INET6;
         server6.sin6_port= htons(port);
         
-        sv=&server6;
         
     }
     
@@ -138,8 +144,9 @@ int connection46(int port, int buffer,int type){
         printf("Create socket successfully.\n");   
     }
     
-    
+    //After creating a socket successfully, bind the sockets.
     if(bind(socket_desc, type==0 ? (struct sockaddr *)&server : (struct sockaddr *)&server6, type==0 ? sizeof(server) : sizeof(server6))<0){
+        //Using ? : to discriminate IPv4 and IPv6.
         printf("Bind fail.\n");
         return 1;
     }
@@ -147,15 +154,19 @@ int connection46(int port, int buffer,int type){
         printf("Bind successfully.\n");
     }
     
+    //Begin to listen incoming connections.
     listen(socket_desc, queue);
     printf("Waiting for IPv%d connection at port %d...\n",type==0 ? 4:6, port);
     int c;
     c = type==0 ? sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6);
     int newsocket;
     while(newsocket = accept(socket_desc, type==0 ? (struct sockaddr *)&client : (struct sockaddr *)&client6, (socklen_t*)&c)){
+        //When there is a incoming TCP request, accept the connection and build the TCP connection.
+        //Here, the process for IPv4 only will accept IPv4 request. And the process for IPv6 only will accept IPv6 request.
         pid_t f=fork();
+        //Build child processes to transfer files.
         if(f==0){
-            puts(type==0 ? "This is a IPv4 connection." : "This is a IPv6 connection.");
+            
             if (newsocket<0){
                 perror("Accept failed.\n");
                 return -1;
@@ -163,8 +174,31 @@ int connection46(int port, int buffer,int type){
             else{
                 printf("Accept successfully.\n");
             }
+            
+            puts(type==0 ? "This is a IPv4 connection." : "This is a IPv6 connection.");
+            
+            if(read(newsocket,&buffer,4)<0){
+                printf("Receiving fail. Keep the original buffer size.\n");
+            }
+            else{
+                printf("Receiving successfully.\n");
+                printf("The present size of buffer is %d.\n",buffer);
+            }
+            
+            //The process will first read the buffer size from client to set the new buffer size and MTU.
+            if(type==1){
+                
+                
+                int mtu=buffer+40;
+                if(mtu>=1280){
+                    if (setsockopt(socket_desc, IPPROTO_IPV6, IPV6_MTU, &mtu, sizeof(mtu)) < 0) {
+                        perror("IPV6_MTU setting failed.");
+                        exit(1);
+                    }
+                }
+            }
     
-    
+            //Then receive the file name which is ready to send.
     
             char msg[30];
             bzero(msg,30);
@@ -176,8 +210,13 @@ int connection46(int port, int buffer,int type){
                 printf("Receiving successfully.\n");
                 printf("%s\n",msg);
             }
+            
+            
+            
+            
     
-    
+            //Then begin to send the file using a function by passing socket descripter, 
+            //file name and buffer size.
             sendfile(newsocket,msg,buffer);
         }
         else if(f<0){
@@ -202,99 +241,41 @@ int main(int argc , char *argv[])
     int pt6=port6,
     pt4=port4,
     bfa[2]={buffersize4,buffersize6};
+    //Initiate variables for setting defaults.
     
     
     int i;
     for(i=1;i<argc;i++){
-        if(strcmp(argv[i],"-p6")==0){
-            pt6=atoi(argv[i+1]);
+        if(i==2){
+            pt6=atoi(argv[i]);
         }
-        if(strcmp(argv[i],"-p4")==0){
-            pt4=atoi(argv[i+1]);
+        if(i==1){
+            pt4=atoi(argv[i]);
         }
-        if(strcmp(argv[i],"-b")==0){
-            bfa[0]=atoi(argv[i+1]);
-            bfa[1]=atoi(argv[i+1]);
+        if(i==3){
+            bfa[0]=atoi(argv[i]);
+            bfa[1]=atoi(argv[i]);
         }
     }
+    //Receive arguments to change defaults.
+    //An example of command of using this program:
+    //./server.o 30001 30002 1460
     
     int pt[2]={pt4,pt6};
     
     
-    // int socket_desc;
-    // struct sockaddr_in server, client;
-    
-    // socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-     
-    // if (socket_desc == -1)
-    // {
-    //     printf("Could not create socket\n");
-    // }
-    // else{
-    //     printf("Create socket successfully.\n");   
-    // }
-    
-    // server.sin_addr.s_addr = INADDR_ANY;
-    // server.sin_family = AF_INET;
-    // server.sin_port= htons(pt);
-    
-    // if(bind(socket_desc, (struct sockaddr *)&server, sizeof(server))<0){
-    //     printf("Bind fail.\n");
-    //     return 1;
-    // }
-    // else{
-    //     printf("Bind successfully.\n");
-    // }
-    
-    // listen(socket_desc, queue);
-    // printf("Waiting...\n");
-    // int c = sizeof(struct sockaddr_in);
-    // int newsocket;
-    // while(newsocket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)){
-    //     int f=fork();
-    //     if(f==0){
-    //         if (newsocket<0){
-    //             perror("Accept failed.\n");
-    //             return -1;
-    //         }
-    //         else{
-    //             printf("Accept successfully.\n");
-    //         }
-    
-    
-    
-    //         char msg[30];
-    //         if(read(newsocket,msg,30)<0){
-    //             printf("Receiving fail.\n");
-    //             return -1;
-    //         }
-    //         else{
-    //             printf("Receiving successfully.\n");
-    //             printf("%s\n",msg);
-    //         }
-    
-    
-    //         sendfile(newsocket,msg,buffer);
-    //     }
-    //     else if(f<0){
-    //         printf("Child process creating failed.\n");
-    //         return -1;
-    //     }
-    
-    // }
-    
-    
-     
-    // return 0;
     
     int processId[2],pid;
+    //Generate one process for listening IPv4 incoming connection and one other process for IPv6. 
     for(i=0;i<2;i++){
         pid=(int)fork();
         if(pid==0){
-            
-            // printf("%d\n",getpid());
             int buffer=bfa[i];
             if(connection46(pt[i],buffer,i)!=0){
+            //When i is 0, there is the process for listening IPv4.
+            //When i is 1, it is for IPv6.
+            //This function will use port number, 
+            //buffer size and the type of IP connection to create connection. 
                 exit(1);
             }
         }
@@ -306,50 +287,17 @@ int main(int argc , char *argv[])
                 case 1:
                     puts("The process to build ipv6 connection generated failed.");
             }
-            
         }
         else{
-        
             processId[i]=pid;
-            
         }
-        
     }
+    
     int status;
     for(i=0;i<2;i++){
-        
         waitpid(processId[i], &status, 0);
-        // switch(status){
-            
-        // }
     }
-    
-    // int pc6;
-    // int pc4=fork();
-    // if(pc4==0){
-    //     if(connection4(pt,buffer)!=0){
-    //         exit(1);
-    //     }
-    // }
-    // else if(pc4<0){
-    //     puts("The process to build ipv4 connection generated failed.");
-    // }
-    // else{
-    //     // pc6=fork();
-    //     // if(pc6==0){
-    //     //     if(connection6(pt,buffer)!=0){
-    //     //         exit(1);
-    //     //     }
-    //     // }
-    //     // else if(pc6<0){
-    //     //     puts("The process to build ipv6 connection generated failed.");
-    //     // }
-    //     // else{
-        
-    //     // }
-    // }
-    
-    printf("%d\n",processId[0]);
+    //Let the main process wait the above two processes.
     
     return 0;
 }
