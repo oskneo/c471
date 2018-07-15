21738f784eaf9fa711a52bfc05a6c52b2ae53bd1
osk666 <osk666@gmail.com>
2018-07-15 07:38:52 +0000

    bash

--- a4/as4.sh
+++ a4/as4.sh
@@ -3,6 +3,10 @@ read -p "Please enter your password:" password
 echo "echo $password" > ./password
 chmod 777 ./password
 
+echo '
 export SSH_ASKPASS="./password"
 export DISPLAY=YOURDOINGITWRONG
 setsid ssh april
+' > ./n16.sh
+chmod 777 ./n16.sh
+./n16.sh
