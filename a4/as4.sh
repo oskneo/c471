8cc83fae45cb4d5fbe92114860775599cac9cedb
osk666 <osk666@gmail.com>
2018-07-15 08:54:32 +0000

    bash

--- a4/as4.sh
+++ a4/as4.sh
@@ -1,5 +1,5 @@
-set MCA1 "224.5.5.5"
-set P1 "5678"
+MCA1="224.5.5.5"
+P1="5678"
 
 read -p "Please enter your password:" password
 
@@ -11,7 +11,7 @@ chmod 777 ./password
 echo '
 export SSH_ASKPASS="./password"
 export DISPLAY=YOURDOINGITWRONG
-setsid ssh april "tshark -i eth1 -a duration:15 -w n16.pcap;"
+setsid ssh april "tshark -i eth1 -a duration:15 -w n16.pcap;exit;"
 ' > ./n16pc.sh
 chmod 777 ./n16pc.sh
 
@@ -31,18 +31,14 @@ echo '
 export SSH_ASKPASS="./password"
 export DISPLAY=YOURDOINGITWRONG
 
-setsid sftp june
-put MCRnet16.mgn
-quit
-
-setsid ssh june
-
-mgen input MCRnet16.mgn
-
-exit
+setsid sftp june << !
+ put MCRnet16.mgn
+ quit
+!
+setsid ssh june "mgen input MCRnet16.mgn;exit;"
 ' > ./n16mcr.sh
 chmod 777 ./n16mcr.sh
 
 
-./n16pc.sh &
-#./n16mcr.sh &
+#./n16pc.sh &
+./n16mcr.sh &
