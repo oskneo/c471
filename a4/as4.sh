967b2aeedfe7c94ee0f4b9786723f37a2bdc6b80
gza21 <gza21@sfu.ca>
2018-07-15 00:35:01 -0700

    bash ssh password

--- a4/as4.sh
+++ a4/as4.sh
@@ -1,4 +1,8 @@
 read -p "Please enter your password:" password
-echo $password
-ssh april
-$password
+
+echo "echo $password" > ./password
+chmod 777 ./password
+
+export SSH_ASKPASS="./password"
+export DISPLAY=YOURDOINGITWRONG
+setsid ssh april
